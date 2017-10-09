30a0ce9ab2688443137fb301b20c3d8d69f136a8
LeroyL <lier6011@163.com>
2017-10-09 16:58:07 +0800

    application update

--- admin/src/main/java/com/jewelry/admin/controller/AdminController.java
+++ admin/src/main/java/com/jewelry/admin/controller/AdminController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.Timestamp;
-import java.util.List;
 
 @Controller
 public class AdminController {
